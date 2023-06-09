b5c0107ca9704b6c634f063f902e785d690ffd98
wassim <wbvb41@gmail.com>
2023-06-09 21:51:15 +0200

    switched the toggle notification function call

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -22,11 +22,8 @@ const Navigation: React.FC = () => {
         </div>
       </div>
       <div className="flex items-center">
-        <button
-          onClick={toggleNotification}
-          className="text-gray-300 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-        >
-          <NotificationsIcon />
+        <button className="text-gray-300 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500">
+          <NotificationsIcon onClick={toggleNotification} />
           {showNotification && <NotificationPopup />}
         </button>
         <button className="text-gray-300 hover:text-white px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500">
