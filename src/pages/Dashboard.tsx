dad23069cb79460c7e642bbe3d6b49ac142d49dc
wassim <wbvb41@gmail.com>
2023-06-11 18:00:46 +0200

    added the dashboard principale page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Sidebar from "../components/Sidebar";
 import Navigation from "../components/navigation/Navigation";
-import Tasks from "../components/Tasks";
+import Tasks from "../components/tasks/Tasks";
 
 const DashboardPage: React.FC = () => {
   return (
