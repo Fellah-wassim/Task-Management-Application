e7c7bedfe09db86707f190b6db1912bddcbba7af
wassim <wbvb41@gmail.com>
2023-06-11 18:01:25 +0200

    added the main page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import Login from "./components/login/Login.tsx";
+import Signup from "./components/signup/Signup.tsx";
+import Dashboard from "./pages/Dashboard";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <Dashboard />
+  // <BrowserRouter>
+  //   <Routes>
+  //     <Route path="/" element={<Home />} />
+  //     <Route path="/tasks" element={<Tasks />} />
+  //     <Route path="/projects" element={<Projects />} />
+  //     <Route path="/settings" element={<Settings />} />
+  //   </Routes>
+  // </BrowserRouter>
+);
