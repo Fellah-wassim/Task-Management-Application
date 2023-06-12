ab28ca3d40dea60875c3b1fd8866c7a4d124a18c
wassim <wbvb41@gmail.com>
2023-06-13 01:30:31 +0200

    updated the navigation bar to be responsive

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -2,17 +2,24 @@ import React, { useState } from "react";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import LogoutIcon from "@mui/icons-material/Logout";
 import NotificationPopup from "./NotificationPopup";
+import { BurgerButton } from "../BurgerButton";
 
-const Navigation: React.FC = () => {
+interface NavigationProps {
+  isOpen: boolean;
+  onBurgerClick: () => void;
+}
+
+const Navigation: React.FC<NavigationProps> = ({ onBurgerClick, isOpen }) => {
   const [showNotification, setShowNotification] = useState(false);
   const toggleNotification = () => {
     setShowNotification(!showNotification);
   };
 
   return (
-    <header className="w-[100%] h-[75px] flex items-center justify-between bg-gray-900 text-white py-4 px-6">
+    <header className="w-[100%] h-[75px] flex items-center justify-between bg-gray-900 text-white py-4 px-6 max-sm:px-2 ">
       <div className="flex items-center">
-        <h1 className="text-xl font-bold">Task Management App</h1>
+        <h1 className="text-xl font-bold max-md:hidden">Task Management App</h1>
+        <BurgerButton isOpen={isOpen} onBurgerClick={onBurgerClick} />{" "}
         <div className="ml-6">
           <input
             type="text"
@@ -28,7 +35,7 @@ const Navigation: React.FC = () => {
         </button>
         <button className="text-gray-300 hover:text-white px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500">
           <LogoutIcon className="m-1" />
-          Logout
+          <span className="max-md:hidden">Logout</span>
         </button>
       </div>
     </header>
