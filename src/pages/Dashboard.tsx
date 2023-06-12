5a111b84414b0063f1a69eee5668a890a64e5637
wassim <wbvb41@gmail.com>
2023-06-13 01:36:59 +0200

    added functionalities for small devices

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useWindowSize } from "../hooks/useWindowSize";
 
 import Sidebar from "../components/Sidebar";
 import Navigation from "../components/navigation/Navigation";
 import Tasks from "../components/tasks/Tasks";
 
 const DashboardPage: React.FC = () => {
+  const [windowSize, setWindowSize] = useState(useWindowSize());
+
+  const [isOpen, setIsOpen] = useState(windowSize.width < 768 ? false : true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+
+      if (windowSize.width > 768) {
+        setIsOpen(true);
+      } else {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [windowSize]);
+
+  const handleToggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <BrowserRouter>
       <div className="flex w-screen h-screen overflow-hidden">
-        <Sidebar />
+        <Sidebar isOpen={isOpen} />
         <div className="w-[100%]">
-          <Navigation />
+          <Navigation isOpen={isOpen} onBurgerClick={handleToggleSidebar} />
           <div className="p-2 h-screen pb-[75px] overflow-scroll">
             <Routes>
               <Route path="/" element={<Home />} />
